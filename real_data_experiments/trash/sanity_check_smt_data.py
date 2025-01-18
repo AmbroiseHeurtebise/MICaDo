1c4344638c84aa9c486ee3115508e56cc5ce0677
AmbroiseHeurtebise <amheurtebise@gmail.com>
2025-01-19 00:55:56 +0100

    various modifs

--- real_data_experiments/trash/sanity_check_smt_data.py
+++ real_data_experiments/trash/sanity_check_smt_data.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.signal import hilbert
+import pickle
 import mne
 import mne_connectivity
 from mne_bids import BIDSPath, read_raw_bids
@@ -24,7 +25,8 @@ FREESURFER_DIR = DATA_DIR / "camcan-mne/freesurfer"
 PARTICIPANTS_FILE = BIDS_ROOT / "participants.tsv"
 SSS_CAL_FILE = DATA_DIR / "camcan-mne/Cam-CAN_sss_cal.dat"
 CT_SPARSE_FILE = DATA_DIR / "camcan-mne/Cam-CAN_ct_sparse.fif"
-SRC = FREESURFER_DIR / "fsaverage/bem/fsaverage-oct-5-src.fif"
+# The file "fsaverage-ico-5-src.fif" has a better definition than "fsaverage-oct-5-src.fif"
+SRC = FREESURFER_DIR / "fsaverage/bem/fsaverage-ico-5-src.fif"
 TRANS_DIR = DATA_DIR / "camcan-mne/trans"
 trans = TRANS_DIR / f"sub-{subject}-trans.fif"
 bem = FREESURFER_DIR / f"{subject}/bem/{subject}-meg-bem.fif"
@@ -84,16 +86,31 @@ if go:
 go = True
 if go:
     # Question: should we pick only gradiometers or magnetometers, or keep both?
-    raw.load_data()
+    raw.load_data(verbose=False)
     # Question: Why using a low-pass filter at 125 Hz? In DriPP paper, they say it's to remove slow drifts, 
     # but I thought that it was the role of high-pass filters...
-    raw.filter(l_freq=None, h_freq=125)
+    # raw.filter(l_freq=None, h_freq=125)
+    # I replaced l_freq=None by l_freq=0.1, as it removed the slow drifts.
+    raw.filter(l_freq=0.1, h_freq=125, verbose=False)
     # Question: Why using a notch-filter? And why attenuating frequencies between 50 and 100 Hz, specifically?
+    # Answer: to remove power line noise and its harmonics.
     raw.notch_filter([50, 100])
     raw = mne.preprocessing.maxwell_filter(
         raw, calibration=SSS_CAL_FILE, cross_talk=CT_SPARSE_FILE, verbose=False)
     # Question: we didn't remove ECG and EOG (probably because Cédric wanted their atoms), 
     # but maybe we should?
+
+# %%
+# Remove ECG and EOG artifacts.
+# In practice, it does not work well but I don't know why.
+go = False
+if go:
+    projs_ecg, _ = compute_proj_ecg(raw, n_grad=1, n_mag=2)
+    eog_channels = mne.pick_types(raw.info, eog=True)
+    eog_ch_names = [raw.ch_names[idx] for idx in eog_channels]
+    projs_eog, _ = compute_proj_eog(raw, n_grad=1, n_mag=2, ch_name=eog_ch_names)
+    raw.add_proj(projs_ecg + projs_eog)
+    raw.apply_proj()
 
 # %%
 # Get events.
@@ -135,7 +152,8 @@ print(f"Number of epochs: {len(epochs)}.")
 # Evoked (only for vizualization).
 evoked = epochs.average()
 # On my laptop, it plots 2 times the same figure (I don't know why).
-evoked.plot(picks="mag", spatial_colors=True, gfp=True)
+# evoked.plot(picks="mag", spatial_colors=True, gfp=True)
+evoked.plot(spatial_colors=True, gfp=True)
 
 # %%
 # Compute the forward and inverse.
@@ -143,9 +161,9 @@ evoked.plot(picks="mag", spatial_colors=True, gfp=True)
 # whereas the transformer file ``trans`` and BEM file ``bem`` are subject-specific.
 # Is it a problem?
 # Remark: we could also use ``trans`` and ``bem`` files from fsaverage., WDYT?
-src = mne.read_source_spaces(SRC)
+src = mne.read_source_spaces(SRC, verbose=False)
 fwd = mne.make_forward_solution(raw.info, trans, src, bem, verbose=False)
-del src
+# del src
 cov = mne.compute_raw_covariance(raw)
 inv = make_inverse_operator(raw.info, fwd, cov, verbose=False)
 del fwd
@@ -155,7 +173,8 @@ del fwd
 # Question: I used the same parameters ``lambda2`` and ``pick_ori`` as in the example you sent,
 # but are they the best choices (especially lambda2)?
 stcs = apply_inverse_epochs(
-    epochs, inv, lambda2=1.0 / 9.0, pick_ori="normal", return_generator=False)
+    epochs, inv, lambda2=1.0 / 9.0, pick_ori="normal", return_generator=False,
+    verbose=False)
 
 # %%
 # Vizualize mean source time course.
@@ -172,47 +191,63 @@ plt.show()
 
 # %%
 # Get labels.
-# Question: we use the "aparc" parcellation that has 68 labels, 
-# does it seem good to you?
-labels = mne.read_labels_from_annot(
-    subject, "aparc", subjects_dir=FREESURFER_DIR)
-
-# %%
-# Unfortunately, because ``src`` comes from fsaverage, we have to morph subject-specific labels to fsaverage.
-# Question: do you validate this approach?
-# Remark: we could also morph source estimate to subject space.
-morphed_labels = mne.morph_labels(
-    labels, subject_to="fsaverage", subject_from=subject,
-    subjects_dir=FREESURFER_DIR)
+# Alex said that we should use the aparc_sub parcellation.
+parcellation = "aparc_sub"
+if parcellation == "aparc":
+    # Get aparc labels from subject.
+    labels = mne.read_labels_from_annot(
+        subject, "aparc", subjects_dir=FREESURFER_DIR)
+    # Unfortunately, because ``src`` comes from fsaverage, 
+    # we have to morph subject-specific labels to fsaverage.
+    morphed_labels = mne.morph_labels(
+        labels, subject_to="fsaverage", subject_from=subject,
+        subjects_dir=FREESURFER_DIR)
+elif parcellation == "aparc_sub":
+    # Get aparc_sub labels from fsaverage.
+    labels = mne.read_labels_from_annot(
+        "fsaverage", "aparc_sub", subjects_dir=FREESURFER_DIR, verbose=False)
+    # Among the 450 labels, 36 do not have vertices in the source space inv["src"].
+    # Indeed, src has 10242 vertices in each hemisphere, whereas inv["src"] only has 
+    # 8428 vertices in the left hemisphere and 7819 in the right hemisphere.
+    # In the following, we remove these 36 labels, so that 414 labels remain.
+    filtered_labels = [
+        label for label in labels
+        if (
+            (label.hemi == "lh" and len(set(label.vertices) & set(inv["src"][0]["vertno"])) > 0)
+            or (label.hemi == "rh" and len(set(label.vertices) & set(inv["src"][1]["vertno"])) > 0)
+        )
+    ]
 
 # %%
 # Compute the average time course across all sources (dipoles) 
 # that belong to each label (region of interest, ROI),
-# so there is one time series per subject and per label.
+# so there is one time series per epoch and per label.
 label_ts = mne.extract_label_time_course(
-    stcs, morphed_labels, inv["src"], return_generator=False, verbose=False)
+    stcs, filtered_labels, inv["src"], return_generator=False, verbose=False)
 print(f"Shape of label_ts: {np.array(label_ts).shape}.")
 
 # %%
 # Vizualize label time series.
 # We averaged over epochs, so there is one curve per label in the parcellation.
-# If plot_only_good == False, we plot the 68 time series.
-# If plot_only_good == True, we only plot the best 10 time series.
-plot_only_good = False
 label_ts_avg = np.mean(label_ts, axis=0)
-# List of 10 (out of the 68) visually selected labels.
-label_idx_good = [1, 7, 11, 24, 28, 34, 36, 38, 40, 64]
-# Maybe also [19, 53, 58, 67] but their peaks are thinner.
-if plot_only_good:
-    label_ts_avg_subset = label_ts_avg[label_idx_good]
-    plt.plot(stcs[0].times, label_ts_avg_subset.T)
-    plt.title("Mean (over epochs) label time series (only good labels)")
-else:
-    plt.plot(stcs[0].times, label_ts_avg.T)
-    plt.title("Mean (over epochs) label time series")
+plt.plot(stcs[0].times, label_ts_avg.T)
+plt.title("Mean (over epochs) label time series")
 plt.xlabel("Time (s)")
 plt.ylabel("Mean amplitude")
 plt.show()
+
+# %%
+# Compute label-specific variance.
+# The general idea is that labels related to neural activity 
+# should have a higher variance.
+var = np.mean(np.var(label_ts, axis=2), axis=0)  # shape (n_labels,)
+label_idx_good = np.argsort(var)[::-1][:20]
+labels_high_variance = [labels[id] for id in label_idx_good]
+
+# %%
+# Save the list of most important labels.
+with open("labels_high_variance.pkl", "wb") as f:
+    pickle.dump(labels_high_variance, f)
 
 # %%
 # We can eventually orthogonalize the time series.
@@ -220,6 +255,27 @@ plt.show()
 go = False
 if go:
     label_ts = mne_connectivity.envelope.symmetric_orth(label_ts)
+
+# %%
+# Band-pass ``label_ts`` at the beta waves range, i.e. between 14 Hz and 30 Hz. 
+# However, it removes the peak.
+label_ts_filtered = mne.filter.filter_data(label_ts, sfreq, 14, 30, verbose=False)
+print(f"Shape of the label_ts_filtered: {np.array(label_ts_filtered).shape}.")
+
+# %%
+label_ts_avg = np.mean(label_ts, axis=0)
+plt.plot(stcs[0].times, label_ts_avg.T)
+plt.title("Mean (over epochs) label time series")
+plt.xlabel("Time (s)")
+plt.ylabel("Mean amplitude")
+plt.show()
+
+# %%
+# Only select a subset of labels.
+# We should select labels now to avoid unuseful computations.
+label_ts_subset = [ts[label_idx_good] for ts in label_ts]
+hilbert_ts_subset = hilbert(label_ts_subset, axis=2)
+envelope_subset = np.abs(hilbert_ts_subset)
 
 # %%
 # Compute the envelope.
@@ -230,20 +286,19 @@ envelope = np.abs(hilbert_ts)
 print(f"Shape of the envelope: {envelope.shape}.")
 
 # %%
+# Crop.
+n_removed = 5
+envelope_cropped = envelope[:, :, n_removed:-n_removed]
+print(f"Shape of the envelope_cropped: {envelope_cropped.shape}.")
+
+# %%
 # Vizualize the average (over epochs) envelope (one curve per label in the parcellation).
 # Question: is it normal for the figure to look like this,
 # especially with peaks at the edges of the figure?
-# If plot_only_good == False, we plot the 68 time series.
-# If plot_only_good == True, we only plot the best 10 time series.
-plot_only_good = True
-envelope_avg = np.mean(envelope, axis=0)
-if plot_only_good:
-    envelope_avg_subset = envelope_avg[label_idx_good]
-    plt.plot(stcs[0].times, envelope_avg_subset.T)
-    plt.title("Mean envelope time series (over epochs; only good labels)")
-else:
-    plt.plot(stcs[0].times, envelope_avg.T)
-    plt.title("Mean envelope time series (over epochs)")
+envelope_avg = np.mean(envelope_cropped, axis=0)
+times = stcs[0].times[n_removed:-n_removed]
+plt.plot(times, envelope_avg.T)
+plt.title("Mean envelope time series (over epochs)")
 plt.xlabel("Time (s)")
 plt.ylabel("Mean amplitude")
 plt.show()
@@ -271,9 +326,9 @@ def batch_average(envelope, n_batches):
         start = end
     return np.array(results)
 
-print(f"Shape of envelope: {envelope.shape}.")
+print(f"Shape of envelope_cropped: {envelope_cropped.shape}.")
 # Average by batch.
-envelope_batch_avg = batch_average(envelope, n_batches=n_batches)
+envelope_batch_avg = batch_average(envelope_cropped, n_batches=n_batches)
 print(f"Shape of envelope_batch_avg: {envelope_batch_avg.shape}.")
 
 # Concatenate batches.
@@ -282,9 +337,14 @@ envelope_concat = envelope_batch_avg.swapaxes(0, 1).reshape(n_labels, -1)
 print(f"Shape of envelope_concat: {envelope_concat.shape}.")
 
 # Downsample timepoints.
+# maybe more a moving average
 factor = int(sfreq // sfreq_envelope)
 envelope_reduced = envelope_concat[:, ::factor]
 print(f"Shape of envelope_reduced: {envelope_reduced.shape}.")
+
+# Only keep the best labels.
+envelope_subset = envelope_reduced[label_idx_good]
+print(f"Shape of envelope_subset: {envelope_subset.shape}.")
 
 # %%
 # Vizualize obtained envelopes.
@@ -292,18 +352,13 @@ print(f"Shape of envelope_reduced: {envelope_reduced.shape}.")
 # Each batch contains time series from tmin=-5 to tmax=5.
 # I don't like the results because I expected a greater peak 
 # in the middle of each batch.
-plot_only_good = True
-if plot_only_good:
-    plt.plot(envelope_concat[label_idx_good].T)
-    plt.title("Reduced envelope time series (only good labels)")
-else:
-    plt.plot(envelope_concat.T)
-    plt.title("Reduced envelope time series")
-batch_length = envelope_concat.shape[1] // n_batches
+plt.plot(envelope_subset.T)
+plt.title("Final data")
+batch_length = envelope_subset.shape[1] // n_batches
 tick_positions = batch_length * np.arange(n_batches) + batch_length // 2
 tick_labels = np.arange(n_batches)
 plt.xticks(tick_positions, tick_labels)
-ymin, ymax = np.min(envelope_concat), np.max(envelope_concat)
+ymin, ymax = np.min(envelope_subset), np.max(envelope_subset)
 for i in range(n_batches+1):
     plt.vlines(x=batch_length*i, ymin=ymin, ymax=ymax, linestyles="--", colors="black")
 plt.xlabel("Batch")
