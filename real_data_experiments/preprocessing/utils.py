407f1ebda5bd96795ffc64b1799f1bf43175fac8
AmbroiseHeurtebise <amheurtebise@gmail.com>
2025-01-27 13:46:58 +0100

    band-pass filter, normalize, change labels, and more

--- real_data_experiments/preprocessing/utils.py
+++ real_data_experiments/preprocessing/utils.py
@@ -22,17 +22,18 @@ TRANS_DIR = DATA_DIR / "camcan-mne/trans"
 
 def process_data_one_subject(
     subject,
-    n_batches=1,
-    sfreq_envelope=None,
-    tmin=-5.,
-    tmax=5.,
-    baseline=(-1.25, -1.0),
-    metadata_tmin=-5.,
-    metadata_tmax=0.,
-    n_crop_edges=None,
-    band_pass_beta=False,
-    moving_avg=True,
+    tmin=-1.5,
+    tmax=3.,
+    baseline=(-1.5, -1.0),
+    fmin=None,
+    fmax=None,
     parcellation="aparc",
+    normalize=True,
+    orthogonalize=False,
+    n_crop_edges=None,
+    moving_avg=True,
+    sfreq_envelope=None,
+    n_batches=1,
     verbose=False,
 ):
     """Process data of one subject.
@@ -49,38 +50,51 @@ def process_data_one_subject(
     with warnings.catch_warnings():
         warnings.filterwarnings("ignore", message=".*Unable to map the following column.*")
         raw = read_raw_bids(bp, verbose=verbose)
-    
+
+    # Pick gradiometers and magnetometers
+    raw.pick(picks=["grad", "mag"])
     # Load data in memory to speed up computations
     raw.load_data(verbose=verbose)
-    # High-pass at 0.1 Hz to remove slow drifts and 
-    # low-pass at 125 Hz because it proves useful
-    raw.filter(l_freq=0.1, h_freq=125, verbose=verbose)
-    # Notch-filter at 50 Hz (and its harmonic 100 Hz)
-    # to remove power line noise
-    raw.notch_filter([50, 100], verbose=verbose)
     # Maxwell filtering using calibration files
     raw = mne.preprocessing.maxwell_filter(
-        raw, calibration=SSS_CAL_FILE, cross_talk=CT_SPARSE_FILE,
-        verbose=verbose)
-    
+        raw, calibration=SSS_CAL_FILE, cross_talk=CT_SPARSE_FILE, verbose=verbose)
+    # Band-pass filter at alpha or beta waves range
+    if fmin is not None or fmax is not None:
+        raw.filter(fmin, fmax, n_jobs=4, verbose=verbose)
+
     # Get events
     sfreq = raw.info["sfreq"]
     all_events, all_event_id = mne.events_from_annotations(
         raw, verbose=verbose)
     metadata, events, event_id = mne.epochs.make_metadata(
-        events=all_events, event_id=all_event_id,
-        tmin=metadata_tmin, tmax=metadata_tmax, sfreq=sfreq,
+        events=all_events, event_id=all_event_id, tmin=tmin, tmax=0., sfreq=sfreq,
         row_events="button", keep_last="audiovis")
-    
+
+    # Add a column to metadata which counts the number of button events within [tmin, tmax]
+    button_times = [
+        event[0] / sfreq for event in all_events if event[2] == all_event_id["button"]
+    ]
+    nb_buttons = []
+    for row_event_time in button_times:
+        count = sum(
+            1
+            for button_time in button_times
+            if tmin <= button_time - row_event_time <= tmax
+        )
+        nb_buttons.append(count)
+    metadata["nb_buttons"] = nb_buttons
+
     # Create epochs
     epochs = mne.Epochs(
-        raw, events, event_id, metadata=metadata, tmin=tmin, tmax=tmax,
-        baseline=baseline, preload=True, verbose=verbose)
-    
+        raw, events, event_id, metadata=metadata, tmin=tmin, tmax=tmax, baseline=baseline,
+        reject=dict(grad=4000e-13, mag=4e-12), preload=True, verbose=verbose)
+
     # Only keep epochs matching the conditions: 
-    # no other button press should occur within ``metadata_tmin`` seconds
-    # and an audiovis stimulus should occur within one second
-    epochs = epochs["event_name == 'button' and audiovis > -1. and button == 0."]
+    # (1) no other button press should occur within [tmin, tmax]
+    # and (2) an audiovis stimulus should occur within one second
+    epochs = epochs["audiovis > -1. and nb_buttons == 1"]
+    n_epochs = len(epochs)
+    print(f"{n_epochs} epochs found for subject {subject}.")
 
     # Compute the forward and inverse operators
     trans = TRANS_DIR / f"sub-{subject}-trans.fif"
@@ -91,7 +105,7 @@ def process_data_one_subject(
     cov = mne.compute_raw_covariance(raw, n_jobs=1, verbose=verbose)
     inv = make_inverse_operator(raw.info, fwd, cov, verbose=verbose)
     del fwd
-    
+
     # Get the source time courses 
     # (inside the cortex; each source corresponds to a dipole)
     stcs = apply_inverse_epochs(
@@ -101,7 +115,7 @@ def process_data_one_subject(
     # Get labels from "fsaverage"
     labels = mne.read_labels_from_annot(
         "fsaverage", parc=parcellation, subjects_dir=FREESURFER_DIR, verbose=verbose)
-    
+
     # Remove labels that do not have any vertex in the source space inv["src"]
     filtered_labels = [
         label for label in labels
@@ -110,14 +124,14 @@ def process_data_one_subject(
             or (label.hemi == "rh" and len(set(label.vertices) & set(inv["src"][1]["vertno"])) > 0)
         )
     ]
-    
+
     # Select some interesting labels
     if parcellation == "aparc":
         # 10 already chosen labels
         label_names = [
-            'precentral-lh', 'postcentral-lh', 'parsopercularis-lh', 'transversetemporal-lh',
-            'pericalcarine-lh', 'precentral-rh', 'postcentral-rh', 'parsopercularis-rh',
-            'transversetemporal-rh', 'pericalcarine-rh']
+            'paracentral-lh', 'precentral-lh', 'postcentral-lh', 'transversetemporal-lh', 
+            'lateraloccipital-lh', 'paracentral-rh', 'precentral-rh', 'postcentral-rh',
+            'transversetemporal-rh', 'lateraloccipital-rh']
     elif parcellation == "aparc_sub":
         # 18 already chosen labels
         label_names = [
@@ -134,29 +148,41 @@ def process_data_one_subject(
         #       'postcentral_3-rh', 'postcentral_4-rh', 'superiorfrontal_1-rh', 'superiorfrontal_2-rh']
     selected_labels = [label for label in filtered_labels if label.name in label_names]
 
-    
     # Compute average time course across all sources (dipoles) 
     # that belong to each label (region of interest, ROI),
     # so there is one time series per epoch and per label.
     label_ts = mne.extract_label_time_course(
         stcs, selected_labels, inv["src"], return_generator=False, verbose=verbose)
-    
-    # Band-pass ``label_ts`` at the beta waves range
-    if band_pass_beta:
-        label_ts = mne.filter.filter_data(
-            label_ts, sfreq, 14, 30, verbose=verbose)
+    label_ts = np.array(label_ts)
+
+    # Eventually normalize the label time series
+    if normalize:
+        baseline_bool = (stcs[0].times >= baseline[0]) & (stcs[0].times <= baseline[1])
+        baseline_idx = np.arange(label_ts.shape[-1])[baseline_bool]
+        baseline_means = np.mean(label_ts[:, :, baseline_idx], axis=-1, keepdims=True)
+        baseline_stds = np.std(label_ts[:, :, baseline_idx], axis=-1, keepdims=True)
+        label_ts = (label_ts - baseline_means) / baseline_stds
+
+    # Eventually orthogonalize
+    if orthogonalize:
+        label_ts = mne_connectivity.symmetric_orth(label_ts)
 
     # Compute the envelope.
     hilbert_ts = hilbert(label_ts, axis=2)
     envelope = np.abs(hilbert_ts)
-    
+
     # Crop envelope to remove edge effects
     if n_crop_edges is not None:
         envelope = envelope[:, :, n_crop_edges:-n_crop_edges]
 
+    # Subtract mean
+    envelope -= np.mean(envelope, axis=-1, keepdims=True)
+
     # Perform batch-averaging
+    if n_epochs < n_batches:
+        raise ValueError(f"There are {n_epochs} epochs for {n_batches} batches.")
     envelope = batch_average(envelope, n_batches=n_batches)
-    
+
     # Downsample time points
     if sfreq_envelope is not None:
         factor = int(sfreq // sfreq_envelope)  # window size
@@ -169,7 +195,7 @@ def process_data_one_subject(
     # Concatenate batches
     n_labels = len(selected_labels)
     envelope = envelope.swapaxes(0, 1).reshape(n_labels, -1)
-    
+
     return envelope, selected_labels
 
 
@@ -262,8 +288,8 @@ def get_participants():
 
     # Only keep participants with clean evoked data (160 out of the 354 with a trans file)
     goods = [
-        'CC110033', 'CC120120', 'CC120182', 'CC120313', 'CC120376', 'CC120550', 'CC120727',
-        'CC120795', 'CC121106', 'CC121111', 'CC121144', 'CC121428', 'CC210051', 'CC210124',
+        'CC110033', 'CC120182', 'CC120313', 'CC120376', 'CC120550', 'CC120727',
+        'CC120795', 'CC121106', 'CC121111', 'CC121144', 'CC121428', 'CC210051',
         'CC210519', 'CC210617', 'CC220151', 'CC220352', 'CC220506', 'CC220518', 'CC220843',
         'CC220901', 'CC221031', 'CC221107', 'CC221220', 'CC221324', 'CC221352', 'CC221565',
         'CC222264', 'CC310086', 'CC310129', 'CC310135', 'CC310361', 'CC310400', 'CC310450',
