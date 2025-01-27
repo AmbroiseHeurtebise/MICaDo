8febc7668e547d1ee71d8a8502a7629be00fefed
AmbroiseHeurtebise <amheurtebise@gmail.com>
2025-01-27 15:26:07 +0100

    plot both hemispheres

--- real_data_experiments/5_plotting/plot_results_camcan.py
+++ real_data_experiments/5_plotting/plot_results_camcan.py
@@ -8,27 +8,18 @@ from pathlib import Path
 
 # %%
 # Parameters
-n_subjects_total = 40
-n_subjects_used = 40
+n_subjects = 3
 parcellation = "aparc"
-hemi = "lh"
 n_arrows = 5
 
-# Load labels
+# Load results
 expes_dir = Path("/storage/store2/work/aheurteb/mvica_lingam/real_data_experiments")
-labels_dir = expes_dir / "data_envelopes"
-with open(labels_dir / f"labels_{parcellation}_{n_subjects_total}_subjects.pkl", "rb") as f:
-    labels_total = pickle.load(f)
-if hemi == "both":
-    labels = labels_total
-else:
-    labels = [label for label in labels_total if label.hemi == hemi]
-
-# Load P, T, and B
-results_dir = Path(expes_dir / f"results/{parcellation}_{n_subjects_used}_subjects_{hemi}")
+results_dir = Path(expes_dir / f"4_results/{parcellation}_{n_subjects}_subjects")
 P = np.load(results_dir / "P.npy")
 T = np.load(results_dir / "T.npy")
 B = np.load(results_dir / "B.npy")
+with open(results_dir / f"labels_{parcellation}_{n_subjects}_subjects.pkl", "rb") as f:
+    labels = pickle.load(f)
 
 # %%
 # Plot average matrix T (should be lower triangular)
@@ -46,9 +37,9 @@ plt.imshow(B_avg, norm=norm, cmap="coolwarm")
 plt.colorbar()
 plt.title("Average adjacency matrix B")
 label_names = [label.name for label in labels]
-ax.set_xticks(np.arange(5))
-ax.set_yticks(np.arange(5))
-ax.set_xticklabels(label_names, rotation=45)
+ax.set_xticks(np.arange(len(label_names)))
+ax.set_yticks(np.arange(len(label_names)))
+ax.set_xticklabels(label_names, rotation=45, ha="right")
 ax.set_yticklabels(label_names)
 plt.show()
 
@@ -69,9 +60,9 @@ norm = TwoSlopeNorm(vmin=np.min(B_avg_subset), vmax=np.max(B_avg_subset), vcente
 plt.imshow(B_avg_subset, norm=norm, cmap="coolwarm")
 plt.colorbar()
 plt.title(f"Average adjacency matrix B ({n_arrows} highest effects)")
-ax.set_xticks(np.arange(5))
-ax.set_yticks(np.arange(5))
-ax.set_xticklabels(label_names, rotation=45)
+ax.set_xticks(np.arange(len(label_names)))
+ax.set_yticks(np.arange(len(label_names)))
+ax.set_xticklabels(label_names, rotation=45, ha="right")
 ax.set_yticklabels(label_names)
 plt.show()
 
