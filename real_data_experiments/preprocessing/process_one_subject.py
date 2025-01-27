41653be5fec1ca5948bd8e36b06a700d11f7106f
AmbroiseHeurtebise <amheurtebise@gmail.com>
2025-01-27 13:47:27 +0100

    update with band-pass filter and other things

--- real_data_experiments/preprocessing/process_one_subject.py
+++ real_data_experiments/preprocessing/process_one_subject.py
@@ -3,15 +3,17 @@ from utils import get_participants, process_data_one_subject, plot_envelope
 
 
 # Parameters
-subject_idx = 0
-n_batches = 10  # used to batch-average epochs
-sfreq_envelope = 10  # used to downsample envelope across timepoints dimension
-metadata_tmin, metadata_tmax = -5., 0. 
-baseline = (-1.25, -1.0)
-tmin, tmax = -5, 5
-n_crop_edges = 5
-moving_avg = True
+subject_idx = 2
+tmin, tmax = -1.5, 3.
+baseline = (-1.5, -1.0)
+fmin, fmax = 8, 27
 parcellation = "aparc"
+normalize = True
+orthogonalize = False
+n_crop_edges = 20
+moving_avg = True
+sfreq_envelope = 10
+n_batches = 40
 
 # Get DataFrame of participants
 participants = get_participants()
@@ -20,23 +22,25 @@ subject = participants.iloc[subject_idx]['participant_id'].replace('sub-', '')
 # Get envelope of shape (n_labels, n_batches*(tmax-tmin)*sfreq_envelope)
 envelope, labels = process_data_one_subject(
     subject,
-    n_batches=n_batches,
-    sfreq_envelope=sfreq_envelope,
     tmin=tmin,
     tmax=tmax,
     baseline=baseline,
-    metadata_tmin=metadata_tmin,
-    metadata_tmax=metadata_tmax,
+    fmin=fmin,
+    fmax=fmax,
+    parcellation=parcellation,
+    normalize=normalize,
+    orthogonalize=orthogonalize,
     n_crop_edges=n_crop_edges,
     moving_avg=moving_avg,
-    parcellation=parcellation,
+    sfreq_envelope=sfreq_envelope,
+    n_batches=n_batches,
 )
 
 # Plot
 plot_envelope(
     envelope,
     n_batches=n_batches,
-    plot_avg=False,
+    plot_avg=True,
     labels=labels,
     save=True,
     subject=subject,
