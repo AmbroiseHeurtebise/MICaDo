3e21f6f7b69c354f78361e17e6e5de97b5797ab0
AmbroiseHeurtebise <amheurtebise@gmail.com>
2025-01-28 19:55:57 +0100

    add file that varies the number of gaussian sources in xaxis

---
+++ simulation_studies/runs/run_simulation_gaussian_sources.py
@@ -0,0 +1,51 @@
+import numpy as np
+import pandas as pd
+import os
+from itertools import product
+from joblib import Parallel, delayed
+from utils import run_experiment
+
+
+# limit the number of jobs
+N_JOBS = 8
+os.environ["OMP_NUM_THREADS"] = str(N_JOBS)
+os.environ["MKL_NUM_THREADS"] = str(N_JOBS)
+os.environ["NUMEXPR_NUM_THREADS"] = str(N_JOBS)
+
+# fixed parameters
+m = 10
+p = 8
+n = 1000
+
+# varying parameters
+nb_gaussian_sources_list = np.arange(p+1)
+nb_seeds = 2
+random_state_list = np.arange(nb_seeds)
+algo_list = ["multiviewica", "shica_j", "shica_ml", "multi_group_direct_lingam", "lingam"]
+
+# run experiment
+nb_expes = len(nb_gaussian_sources_list) * len(random_state_list) * len(algo_list)
+print(f"\nTotal number of experiments : {nb_expes}")
+print("\n###################################### Start ######################################")
+dict_res = Parallel(n_jobs=N_JOBS)(
+    delayed(run_experiment)(
+        m=m,
+        p=p,
+        n=n,
+        nb_gaussian_sources=nb_gaussian_sources,
+        random_state=random_state,
+        ica_algo=ica_algo,
+    ) for nb_gaussian_sources, random_state, ica_algo
+    in product(nb_gaussian_sources_list, random_state_list, algo_list)
+)
+print("\n################################ Obtained DataFrame ################################")
+df = pd.DataFrame(dict_res)
+print(df)
+
+# save dataframe
+results_dir = "/storage/store2/work/aheurteb/mvica_lingam/simulation_studies/results/"
+parent_dir = "gaussian_sources_in_xaxis/"
+save_name = f"DataFrame_with_{nb_seeds}_seeds_and_4_metrics"
+save_path = results_dir + parent_dir + save_name
+df.to_csv(save_path, index=False)
+print("\n####################################### End #######################################")
