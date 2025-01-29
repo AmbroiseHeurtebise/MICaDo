3cb29e6a38e23a90b7c1efec191bad8e7f080401
AmbroiseHeurtebise <amheurtebise@gmail.com>
2025-01-29 18:23:31 +0100

    fix error with chosen metrics

--- simulation_studies/plotting/plot_simulation_gaussian_sources.py
+++ simulation_studies/plotting/plot_simulation_gaussian_sources.py
@@ -18,48 +18,55 @@ rc = {
 plt.rcParams.update(rc)
 
 # parameters 
-nb_seeds = 3
-errors = ["error_B", "error_T", "error_P"]  # ["amari_distance"]
-error_names = ["Error on B", "Error on T", "Error rate on P"]  # ["Amari distance"]
+nb_seeds = 20
+errors = ["error_B", "error_T", "error_P"]
+error_names = [r"Error on $B^i$", r"Error on $T^i$", r"Error rate on $P$"]
 estimator = "mean"
-labels = ['ShICA-ML-LiNGAM', 'ShICA-J-LiNGAM', 'LiNGAM', 'MultiGroupDirectLiNGAM']
-hue_order = ["shica_ml", "shica_j", "lingam", "multi_group_direct_lingam"]
+labels = ['ShICA-ML-LiNGAM', 'ShICA-J-LiNGAM', 'LiNGAM', 'MultiGroupDirectLiNGAM', 'MVICA-LiNGAM']
+hue_order = ["shica_ml", "shica_j", "lingam", "multi_group_direct_lingam", "multiviewica"]
 
 # read dataframe
 results_dir = "/storage/store2/work/aheurteb/mvica_lingam/simulation_studies/results/"
 parent_dir = "gaussian_sources_in_xaxis/"
-save_name = f"/DataFrame_with_{nb_seeds}_seeds_and_4_metrics"
+save_name = f"DataFrame_with_{nb_seeds}_seeds_and_4_metrics"
 save_path = results_dir + parent_dir + save_name
 df = pd.read_csv(save_path)
 
 # subplots
-fig, axes = plt.subplots(1, 3, figsize=(12, 2), sharex=True)
-for i, ax in enumerate(axes.flat):
-    y = errors[i // 3]
+fig, axes = plt.subplots(1, 3, figsize=(12, 3), sharex=True)
+for i, ax in enumerate(axes):
+    y = errors[i]
     sns.lineplot(
         data=df, x="nb_gaussian_sources", y=y, linewidth=2.5, hue="ica_algo", ax=ax, 
         errorbar=('ci', 95), hue_order=hue_order, style_order=hue_order, style="ica_algo",
-        dashes=['', '', (2, 2), (2, 2)])
+        dashes=['', '', (2, 2), (2, 2), ''])
     if i != 2:
         ax.set_yscale("log")
+    ax.set_xticks(np.unique(df["nb_gaussian_sources"]))
+    ax.set_xticklabels(np.unique(df["nb_gaussian_sources"]))
+    ax.set_xlabel("")
+    ax.set_ylabel("")
     ax.set_title(error_names[i], fontsize=fontsize)
     ax.grid(which='both', linewidth=0.5, alpha=0.5)
     ax.get_legend().remove()
-label = fig.supxlabel("Number of samples", fontsize=fontsize)
-label.set_position((0.5, 0.055))
+xlabel = fig.supxlabel("Number of Gaussian sources", fontsize=fontsize)
+xlabel.set_position((0.5, 0.066))
+ylabel = fig.supylabel("Errors", fontsize=fontsize)
+ylabel.set_position((0.02, 0.5))
 plt.tight_layout()
 plt.subplots_adjust(hspace=0.15)
 # legend
-palette = sns.color_palette()[:4]
+palette = sns.color_palette()[:5]
 legend_styles = [
     Line2D([0], [0], color=palette[0], linewidth=2.5, linestyle='-'),
     Line2D([0], [0], color=palette[1], linewidth=2.5, linestyle='-'),
     Line2D([0], [0], color=palette[2], linewidth=2.5, linestyle='--'),
     Line2D([0], [0], color=palette[3], linewidth=2.5, linestyle='--'),
+    Line2D([0], [0], color=palette[4], linewidth=2.5, linestyle='-'),
 ]
 fig.legend(
-    legend_styles, labels, bbox_to_anchor=(0.5, 1.03), loc="center",
-    ncol=4, borderaxespad=0., fontsize=fontsize
+    legend_styles, labels, bbox_to_anchor=(0.5, 1.07), loc="center",
+    ncol=3, borderaxespad=0., fontsize=fontsize
 )
 
 # save figure
