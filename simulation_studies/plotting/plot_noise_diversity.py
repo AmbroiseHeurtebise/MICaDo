bb3cb2ce181e235d62ed73c1c4fe14a3999a8054
AmbroiseHeurtebise <amheurtebise@gmail.com>
2025-03-29 14:08:40 +0100

    update file

--- simulation_studies/plotting/plot_noise_diversity.py
+++ simulation_studies/plotting/plot_noise_diversity.py
@@ -1,4 +1,3 @@
-# %%
 import pandas as pd
 import numpy as np
 from pathlib import Path
@@ -20,9 +19,8 @@ rc = {
 plt.rcParams.update(rc)
 
 # parameters 
-nb_seeds = 2
+nb_seeds = 10
 metric = "error_B"  # or "error_T", "error_P_exact", "error_P_spearmanr", "amari_distance"
-# include_multiviewica = True
 
 # read dataframe
 results_dir = "/storage/store2/work/aheurteb/MICaDo/simulation_studies/results/results_noise_diversity/"
@@ -30,10 +28,49 @@ save_name = f"DataFrame_with_{nb_seeds}_seeds"
 save_path = results_dir + save_name
 df = pd.read_csv(save_path)
 
-# %%
+# metric name
+if metric == "error_B":
+    metric_name = r"Error on $B^i$"
+elif metric == "error_T":
+    metric_name = r"Error on $T^i$"
+elif metric == "error_P_exact":
+    metric_name = r"Error on $P$"
+elif metric == "error_P_spearmanr":
+    metric_name = "Spearman's rank\ncorrelation on" + r" $P$"
+elif metric == "amari_distance":
+    metric_name = "Amari distance"
+
+# labels, dashes and curves order
+labels = ['MICaDo-ML', 'MICaDo-J']
+dashes = ['', '']
+hue_order = ["shica_ml", "shica_j"]
+
+# plot
+fig, ax = plt.subplots(figsize=(6, 3))
 sns.lineplot(
     data=df, x="nb_equal_variances", y=metric, linewidth=2.5, hue="ica_algo", estimator=np.median,
-    errorbar=('ci', 95), style="ica_algo",
-    markers=True)
+    errorbar=('ci', 95), hue_order=hue_order, style_order=hue_order, style="ica_algo",
+    dashes=dashes, markers=True)
+ax.set_yscale("log")
+ax.set_xlabel("# views with equal variances", fontsize=fontsize)
+ax.set_ylabel(metric_name, fontsize=fontsize)
+ax.grid(which='both', linewidth=0.5, alpha=0.5)
+ax.get_legend().remove()
 
-# %%
+# legend
+palette = sns.color_palette()[:2]
+legend_styles = [
+    Line2D([0], [0], color=palette[0], linewidth=2.5, linestyle='-', marker='o', 
+           markeredgecolor="white", markersize=6),
+    Line2D([0], [0], color=palette[1], linewidth=2.5, linestyle='-', marker='X', 
+           markeredgecolor="white", markersize=7),
+]
+fig.legend(
+    legend_styles, labels, bbox_to_anchor=(0.5, 1.03), loc="center",
+    ncol=2, borderaxespad=0., fontsize=fontsize
+)
+
+# save figure
+figures_dir = Path("/storage/store2/work/aheurteb/MICaDo/simulation_studies/figures")
+plt.savefig(figures_dir / f"simulation_noise_diversity.pdf", bbox_inches="tight")
+plt.show()
