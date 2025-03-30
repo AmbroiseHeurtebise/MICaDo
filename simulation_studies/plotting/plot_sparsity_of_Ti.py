5b7b3395af70f3f745d379f7f5d28361f09d3107
AmbroiseHeurtebise <amheurtebise@gmail.com>
2025-03-30 20:24:25 +0200

    cosmetic changes

--- simulation_studies/plotting/plot_sparsity_of_Ti.py
+++ simulation_studies/plotting/plot_sparsity_of_Ti.py
@@ -9,7 +9,7 @@ warnings.filterwarnings("ignore", category=FutureWarning, module="seaborn")
 
 
 # matplotlib style
-fontsize = 15
+fontsize = 14
 rc = {
     "font.size": fontsize,
     "xtick.labelsize": fontsize,
@@ -20,7 +20,7 @@ plt.rcParams.update(rc)
 
 # parameters 
 nb_seeds = 20
-metrics = ["error_B_abs", "error_T_abs", "error_P_spearmanr"]  # or "error_B", "error_P_exact", "amari_distance"
+metric = "error_P_spearmanr"  # or "error_B", "error_P_exact", "amari_distance"
 
 # read dataframe
 results_dir = "/storage/store2/work/aheurteb/MICaDo/simulation_studies/results/results_sparsity_of_Ti/"
@@ -29,18 +29,16 @@ save_path = results_dir + save_name
 df = pd.read_csv(save_path)
 
 # metric names
-metric_names = []
-for metric in metrics:
-    if metric == "error_B" or metric == "error_B_abs":
-        metric_names.append(r"Error on $B^i$")
-    elif metric == "error_T" or metric == "error_T_abs":
-        metric_names.append(r"Error on $T^i$")
-    elif metric == "error_P_exact":
-        metric_names.append(r"Error on $P$")
-    elif metric == "error_P_spearmanr":
-        metric_names.append("Spearman's rank\ncorrelation on" + r" $P$")
-    elif metric == "amari_distance":
-        metric_names.append("Amari distance")
+if metric == "error_B" or metric == "error_B_abs":
+    metric_name = r"Error on $B^i$"
+elif metric == "error_T" or metric == "error_T_abs":
+    metric_name = r"Error on $T^i$"
+elif metric == "error_P_exact":
+    metric_name = r"Error on $P$"
+elif metric == "error_P_spearmanr":
+    metric_name = "Spearman's rank\ncorrelation of the\ncausal ordering(s)"
+elif metric == "amari_distance":
+    metric_name = "Amari distance"
 
 # labels, dashes, curves order and titles
 labels = ['MICaDo-ML', 'MICaDo-J']
@@ -49,28 +47,28 @@ hue_order = ["shica_ml", "shica_j"]
 titles = ["Multiple causal orderings", "Shared causal ordering"]
 
 # subplots
-fig, axes = plt.subplots(3, 2, figsize=(8.5, 6), sharex=True)
+fig, axes = plt.subplots(1, 2, figsize=(8, 2.7), sharex=True, sharey=True)
 for i, ax in enumerate(axes.flat):
-    metric = metrics[i // 2]
     shared_causal_ordering = bool(i % 2)
     data = df[df["shared_causal_ordering"] == shared_causal_ordering]
     sns.lineplot(
         data=data, x="nb_zeros_Ti", y=metric, linewidth=2.5, hue="ica_algo", estimator=np.median,
         errorbar=('ci', 95), hue_order=hue_order, style_order=hue_order, style="ica_algo", ax=ax,
         dashes=dashes, markers=True)
-    if i // 2 != 2:
-        ax.set_yscale("log")
-    # if i % 2 == 0:
-    ax.set_ylabel(metric_names[i // 2])
-    # title, grid, and legend
-    if i // 2 == 0:
-        ax.set_title(titles[i], fontsize=fontsize)
     ax.set_xlabel("")
+    if i == 0:
+        ylabel = ax.set_ylabel(metric_name)
+        ax.yaxis.set_label_coords(-0.12, 0.5)
+    ax.set_title(titles[i], fontsize=fontsize)
     ax.grid(which='both', linewidth=0.5, alpha=0.5)
     ax.get_legend().remove()
+# ax.set_ylim(-1.1, 1.1)
+# ax.set_yticks([-1, 0, 1])
+# ax.set_yticklabels([-1, 0, 1])
+ax.set_yticks([0, 1])
+ax.set_yticklabels([0, 1])
 xlabel = fig.supxlabel(r"# sparse entries in each $T^i$", fontsize=fontsize)
-xlabel.set_position((0.5, 0.07))
-plt.gcf().align_labels()
+xlabel.set_position((0.5, 0.16))
 plt.tight_layout()
 plt.subplots_adjust(hspace=0.15)
 
@@ -83,7 +81,7 @@ legend_styles = [
            markeredgecolor="white", markersize=7),
 ]
 fig.legend(
-    legend_styles, labels, bbox_to_anchor=(0.5, 1.02), loc="center",
+    legend_styles, labels, bbox_to_anchor=(0.5, 1.03), loc="center",
     ncol=2, borderaxespad=0., fontsize=fontsize
 )
 
